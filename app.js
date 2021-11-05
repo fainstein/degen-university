1303b27ff9cb77d2d73ff60166f1f39501b2a76a
fainstein <72775588+fainstein@users.noreply.github.com>
2021-11-05 11:51:57 -0300

    frontend finished

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ var _ = require('lodash');
 const multer = require('multer');
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: function(req, file, cb) {
     cb(null, './images/');
   },
-  filename: function (req, file, cb) {
+  filename: function(req, file, cb) {
     cb(null, Date.now() + file.originalname)
   }
 })
@@ -43,8 +43,7 @@ app.use(express.urlencoded({
   extended: true
 }));
 
-
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 app.use('/images', express.static("images"));
 
 // MONGOOSE MONGODB CONNECT
@@ -84,140 +83,140 @@ filtersArray = [{
   }
 ];
 
+const userSchema = mongoose.Schema({
+  username: String,
+  password: String
+});
+
 const newsSchema = mongoose.Schema({
-title: String,
-description: String,
-image: String
+  title: String,
+  description: String,
+  image: String
 });
 
 const deGenSchema = mongoose.Schema({
-alias: String,
-twitter: String,
-substack: String,
-avatarImg: String
+  alias: String,
+  twitter: String,
+  substack: String,
+  avatarImg: String
 });
 
 const commentSchema = mongoose.Schema({
-author: String,
-content: String,
-date: Date
+  author: String,
+  content: String,
+  date: Date
 });
 
 
 const deGenClassSchema = mongoose.Schema({
-deGen: deGenSchema,
-title: String,
-shortDescription: String,
-description: [String],
-tags: [String],
-comments: [commentSchema]
+  deGen: deGenSchema,
+  title: String,
+  shortDescription: String,
+  description: [String],
+  tags: [String],
+  comments: [commentSchema]
 });
 
 const shopItemSchema = mongoose.Schema({
   image: String,
-title: String,
-content: String,
-price: String,
-tags: [String],
-link: String
+  title: String,
+  content: String,
+  price: String,
+  tags: [String],
+  link: String
 });
 
 
-
 // MONGOOSE MODELS
+const User = mongoose.model("User", userSchema);
 const News = mongoose.model("News", newsSchema);
 const DeGen = mongoose.model("DeGen", deGenSchema);
 const Comment = mongoose.model("Comment", commentSchema);
 const DeGenClass = mongoose.model("DeGenClass", deGenClassSchema);
 const ShopItem = mongoose.model("ShopItem", shopItemSchema);
 
-//ESTO ES PROVISORIO HASTA ARMAR LOS POST METHODS
-const new1 = new News ({
-  title:"Lorem ipsum 1",
-  description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  image: "BowTiedOx.jpg"
-});
-
-const new2 = new News ({
-  title:"Lorem ipsum 2",
-  description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  image: "angryBird.jpeg"
-});
-
-const new3 = new News ({
-  title:"Lorem ipsum 3",
-  description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  image: "reptile.jpg"
-});
-
-const newsHardcoded = [new1, new2, new3];
-
-const deGen1 = new DeGen({
-alias: "BowTiedOx",
-twitter: "BowTiedOx",
-twitterLink: "https://twitter.com/",
-substack: "BowTiedOx",
-substackLink: undefined,
-avatarImg: "BowTiedOx.jpg"
-});
-
-deGen1.substackLink = "https://" + deGen1.substack + ".substack.com/subscribe";
-
-const comment1 = new Comment({
-author: "BowTiedBull",
-content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-date: new Date()
-});
-
-const class1 = new DeGenClass ({
-  deGen: deGen1,
-  title: "Why you must train",
-  shortDescription: "Here BowTiedOx, 5x DeGen Island bodybuilding champion is going to teach you the basics of training and it's importance. Here BowTiedOx, 5x DeGen Island bodybuilding champion is going to teach you the basics of training and it's importance",
-  description: ["Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."],
-  tags: ["Fitness", "Diet", "DeFi"],
-  comments: [comment1, comment1]
-})
-
-const shopItem1 = new ShopItem({
-image: "reptile.jpg",
-title: "OX's Fat Loss Program",
-content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-price: "255 U$TT",
-tags: ["Fitness", "Diet", "DeFi"],
-link: "https://bowtiedox.substack.com/subscribe"
-});
-
 // GET METHODS
 
-app.get("/", function (req, res) {
-  res.render("index", {newsArray: newsHardcoded});
+app.get("/", function(req, res) {
+
+  News.find((err, news) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", {
+        newsArray: news
+      });
+    }
+  })
+
 });
 
-app.get("/classes", function (req, res) {
-  res.render("classes", {classesArray: [class1, class1, class1], filters: filtersArray});
+app.get("/classes", function(req, res) {
+
+  DeGenClass.find((err, deGenClasses) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("classes", {
+        classesArray: deGenClasses,
+        filters: filtersArray
+      });
+    }
+  });
 });
 
-app.get("/class", function (req, res) {
-  res.render("class", {deGenClass: class1});
+app.get("/class/:classId", function(req, res) {
+
+  DeGenClass.findOne({
+    id: req.params.classId
+  }, (err, singleClass) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("class", {
+        deGenClass: singleClass
+      });
+    }
+  });
 });
 
-app.get("/shop", function (req, res) {
-  res.render("shop", {shopItemArray: [shopItem1, shopItem1, shopItem1, shopItem1, shopItem1, shopItem1], filters: filtersArray});
+app.get("/shop", function(req, res) {
+
+  ShopItem.find((err, shopItems) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("shop", {
+        shopItemArray: shopItems,
+        filters: filtersArray
+      });
+    }
+  });
 });
 
-app.get("/deGens", function (req, res) {
-  res.render("deGens", {deGensArray: [deGen1, deGen1, deGen1, deGen1, deGen1, deGen1]});
+app.get("/deGens", function(req, res) {
+
+  DeGen.find((err, deGens) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("deGens", {
+        deGensArray: deGens
+      });
+    }
+  });
+
 });
 
-app.get("/about", function (req, res) {
+app.get("/about", function(req, res) {
   res.render("about");
 });
 
-app.get("/contact", function (req, res) {
+app.get("/contact", function(req, res) {
   res.render("contact");
 });
 
-app.get("/login", function (req, res) {
+app.get("/login", function(req, res) {
   res.render("login");
 });
 
@@ -229,6 +228,6 @@ app.get("/login", function (req, res) {
 // })
 
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server succesfully running");
 });
